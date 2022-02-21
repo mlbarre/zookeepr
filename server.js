b6d57712798d49e4d51d4a20ab3b0d0862c9f667
mlbarre <mlbarrera1538@gmail.com>
2022-02-21 09:20:37 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
 const express = require('express');
-const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const fs = require('fs');
-const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
